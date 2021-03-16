2602987a2056581d5e6d8789e4bd70185f8e51ec
Ashutosh kumar <ashutoshkr26.cse.iimtgn@gmail.com>
2021-03-16 21:05:17 +0530

    initial commit

---
+++ app/src/main/java/com/app/shopping_app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.app.shopping_app
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
